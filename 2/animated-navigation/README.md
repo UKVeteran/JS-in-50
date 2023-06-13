b595f9045e12e135cda0954409fed500723ab28e
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-06-13 10:46:18 +0100

    Add files via upload

---
+++ 2/animated-navigation/README.md
@@ -0,0 +1,4 @@
+# Animated Navigation 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/JAMAZON/pen/WNYQjge](https://codepen.io/JAMAZON/pen/WNYQjge).
+
---
+++ 2/animated-navigation/src/script.js
@@ -0,0 +1,4 @@
+onst toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
